9c63e0e80c7fe6020091507fc8d7ef00f682a5d7
Viktor <feniks4913@yandex.ru>
2017-10-01 15:07:30 +0300

    refactor

--- public/index.php
+++ public/index.php
@@ -2,55 +2,42 @@
 
 // require composer auto loading
 require __DIR__ . '/../vendor/autoload.php';
-$orders = [];
+
 /**
  * Create array orders
  */
+$orders = [];
+
 foreach (range(0, rand(9, 99)) as $num) {
     $orders[$num] = (new \App\Order)->make(rand(0, 30), rand(10, 30), [rand(-1000, 1000), rand(-1000, 1000)]);
 }
+
 $orders = (new \App\Cook)->processing($orders);
 $orders = (new \App\Manager)->processing($orders);
-(new \App\Route)->delivery($orders);
-$orderList=$orders;
+
+$orderList = $orders;
+
 foreach ($orderList as $ordered) {
     foreach ($ordered as $order) {
-        echo "\n id заказа: №" . $order->orderId;
-        echo "\n время поступления: " . $order->timeSend . "ед";
-        echo "\n время готовки: " . $order->timeCook . "ед";
-        echo "\n Координата Х: " . $order->coordinates[0];
-        echo "\n Координата Y: " . $order->coordinates[1] . "\n\n\n";
+        (new \App\Logger)->single([
+            'id' => $order->orderId,
+            'время поступления' => $order->timeSend,
+            'время готовки' => $order->timeCook,
+            'Координата Х' => $order->coordinates[0],
+            'Координата Y' => $order->coordinates[1],
+        ]);
     }
 }
-$orders = (new \App\Order)->delivery($orders);
-$i=0;
-$routeList=$orders;
-foreach ($routeList as $routes) {
-$i++;
-//die(var_dump($routes[0]->timeDelivery));
-    if($routes[0]) {
-        echo "\nЭто " . $i . " цикл доставки ";
-        echo "\nПервый заказ ";
-        echo "\n id заказа: №" . $routes[0]->orderId;
-        echo "\n Время доставки: " . $routes[0]->timeDelivery;
-    }
-    if($routes[1]) {
-        echo "\nВторой заказ ";
-        echo "\n id заказа: №" . $routes[1]->orderId;
-        echo "\n Время доставки: " . $routes[1]->timeDelivery;
-    }
-    if($routes[2]){
-        echo "\nТретий заказ ";
-        echo "\n id заказа: №" . $routes[2]->orderId;
-        echo "\n Время доставки: " . $routes[2]->timeDelivery;
-    }
+$routeList = (new \App\Order)->delivery($orders);
 
-
-//    foreach ($routes as $route) {
-//        echo "\n id заказа: №" . $route->orderId;
-//        echo "\n время поступления: " . $route->timeSend . "ед";
-//        echo "\n время готовки: " . $route->timeCook . "ед";
-//        echo "\n Координата Х: " . $route->coordinates[0];
-//        echo "\n Координата Y: " . $route->coordinates[1] . "\n\n\n";
-//    }
+foreach ($routeList as $key => $routes) {
+    (new \App\Logger)->single([
+        'Маршрут №' => $key,
+    ]);
+    foreach($routes as $order) {
+        (new \App\Logger)->single([
+            'id заказа' => $order->orderId,
+            'время доставки заказа' => $order->timeSend,
+        ]);
+    }
 }
