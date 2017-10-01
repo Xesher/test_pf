3002007eb4a0bac37059af601aefd525823401da
Xesh_ <feniks4913@yandex.ru>
2017-10-01 12:49:19 +0300

    Add class Order

--- src/Order.php
+++ src/Order.php
@@ -1,7 +1,33 @@
 <?php declare(strict_types=1);
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 01.10.2017
- * Time: 12:43
- */
+
+namespace App;
+
+class Order
+{
+    private static $id = 0;
+    public $orderId;
+    public $timeSend;
+    public $timeCook;
+    public $coordinates = [0, 0];
+    public $vector;
+    public $processed_at;
+    public $lengthVector;
+    public $timeDelivery;
+
+
+    /**
+     * @param int $timeSend
+     * @param int $timeCook
+     * @param array $coordinates
+     * @return $this
+     */
+    public function make(int $timeSend, int $timeCook, array $coordinates)
+    {
+        $this->orderId = self::$id++;
+        $this->timeSend = $timeSend;
+        $this->timeCook = $timeCook;
+        $this->coordinates[0] = $coordinates[0];
+        $this->coordinates[1] = $coordinates[1];
+        return $this;
+    }
+}
