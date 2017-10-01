3d0f56e5f3feefd2750f22aebed0426ddd269f43
Xesh_ <feniks4913@yandex.ru>
2017-10-01 12:45:58 +0300

    first commit

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php declare(strict_types=1);
+
+// require composer auto loading
+require __DIR__ . '/../vendor/autoload.php';
---
+++ src/Manager.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 01.10.2017
+ * Time: 12:43
+ */
