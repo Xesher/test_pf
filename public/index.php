189cb285609fa73f7b0caf406a3fd5902a6c89ff
Xesh_ <feniks4913@yandex.ru>
2017-10-01 13:02:32 +0300

    Route class , output the orders

--- public/index.php
+++ public/index.php
@@ -11,3 +11,14 @@ foreach (range(0, rand(9, 99)) as $num) {
 }
 $orders = (new \App\Cook)->processing($orders);
 $orders = (new \App\Manager)->processing($orders);
+(new \App\Route)->delivery($orders);
+$routeList=$orders;
+foreach ($routeList as $routes) {
+    foreach ($routes as $route) {
+        echo "\n id заказа: №" . $route->orderId;
+        echo "\n время поступления: " . $route->timeSend . "ед";
+        echo "\n время готовки: " . $route->timeCook . "ед";
+        echo "\n Координата Х: " . $route->coordinates[0];
+        echo "\n Координата Y: " . $route->coordinates[1] . "\n\n\n";
+    }
+}
--- src/Route.php
+++ src/Route.php
@@ -2,3 +2,57 @@
 
 namespace App;
 
+class Route
+{
+    /**
+     * @param array $pointOne
+     * @param array $pointTwo
+     * @return float
+     */
+    function lengthVector(array $pointOne, array $pointTwo)
+    {
+        return round(sqrt(pow(($pointTwo[0] - $pointOne[0]), 2) + pow(($pointTwo[1] - $pointOne[1]), 2)), 2);
+    }
+
+    /**
+     * @param $orders
+     * @return mixed
+     */
+//    public function processing($orders)
+//    {
+//        $i = 0;
+//        foreach ($orders as $order) {
+//            $i++;
+//            $order[$i]->processed_at = round($order->vector/ 60, 0);
+//        }
+//        return $orders->processed_at;
+//    }
+
+    /**
+     * @param $orders
+     */
+    public function delivery($orders)
+    {
+        $a = [0, '-'];
+        $b = [9999, '-'];
+        $c = [9999, '-'];
+        $d = [9999, '-'];
+
+        $startPoint = [0, 0];
+//        var_dump($orders[0]);
+//        dot A
+        $ab = $this->lengthVector($startPoint, $orders[0][0]->coordinates);
+        $ac = $this->lengthVector($startPoint, $orders[0][1]->coordinates);
+//        dot B
+        $bc = $this->lengthVector($orders[0][0]->coordinates, $orders[0][1]->coordinates);
+        $bd = $this->lengthVector($orders[0][0]->coordinates, $orders[0][2]->coordinates);
+//        dot C
+        $cb = $this->lengthVector($orders[0][1]->coordinates, $orders[0][0]->coordinates);
+        $cd = $this->lengthVector($orders[0][1]->coordinates, $orders[0][2]->coordinates);
+//        dot D
+        $db = $this->lengthVector($orders[0][2]->coordinates, $orders[0][0]->coordinates);
+        $dc = $this->lengthVector($orders[0][2]->coordinates, $orders[0][1]->coordinates);
+//        echo('A=' . $ab . '/' . $ac . '/B=' . $bc . '/' . $bd . '/C=' . $cb . '/' . $cd . '/D=' . $db . '/' . $dc . '/');
+
+    }
+}
